96901a0989bc214a57fa5d5a2581f5d1c2bf8420
OsMax <www.osmax@gmail.com>
2024-08-29 16:19:55 +0300

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,68 @@
 import { NavLink } from "react-router-dom";
+import css from "./Header.module.css";
+import { ReactComponent as Burger } from "../../assets/svgImg/burger.svg";
+import { ReactComponent as Close } from "../../assets/svgImg/menu_close.svg";
+import { ReactComponent as Logo } from "../../assets/svgImg/logo.svg";
+import { useState } from "react";
+// import Menu from "../Menu/Menu";
 
+const setAtive = ({ isActive }) => (isActive ? css.active : css.navLink);
 const Header = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
     <header>
       <section>
-        <NavLink to="/">Some icon</NavLink>
-        <nav>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/skills">Skills</NavLink>
-          <NavLink to="/projects">Projects</NavLink>
-          <NavLink to="/about">About</NavLink>
-          <NavLink to="/contacts">Contacts</NavLink>
+        <nav className={css.navContainer}>
+          <NavLink className={css.logo} to="/">
+            <Logo width="24" height="24" />
+            MaksOs
+          </NavLink>
+          <div className={`${css.menu} ${showMenu ? css.showMenu : ""}`}>
+            <Close
+              width="24"
+              height="24"
+              className={css.closeBtn}
+              onClick={() => setShowMenu(false)}
+            />
+            <NavLink
+              className={setAtive}
+              to="/"
+              onClick={() => setShowMenu(false)}
+            >
+              Home
+            </NavLink>
+            <NavLink
+              className={setAtive}
+              to="/skills"
+              onClick={() => setShowMenu(false)}
+            >
+              Skills
+            </NavLink>
+            <NavLink
+              className={setAtive}
+              to="/projects"
+              onClick={() => setShowMenu(false)}
+            >
+              Projects
+            </NavLink>
+            <NavLink
+              className={setAtive}
+              to="/about"
+              onClick={() => setShowMenu(false)}
+            >
+              About
+            </NavLink>
+            <NavLink
+              className={setAtive}
+              to="/contacts"
+              onClick={() => setShowMenu(false)}
+            >
+              Contacts
+            </NavLink>
+          </div>
+          <Burger width="24" height="24" onClick={() => setShowMenu(true)} />
+          {/* <Menu /> */}
         </nav>
       </section>
     </header>
