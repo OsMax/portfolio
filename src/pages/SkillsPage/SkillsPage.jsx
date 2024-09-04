1b64bd43628399b2843a2d3d6fe759d0e4763332
OsMax <www.osmax@gmail.com>
2024-09-04 16:25:24 +0300

    Update SkillsPage.jsx

--- src/pages/SkillsPage/SkillsPage.jsx
+++ src/pages/SkillsPage/SkillsPage.jsx
@@ -1,8 +1,15 @@
+import css from "./SkillsPage.module.css";
+import { ReactComponent as MySkills } from "../../assets/skills/mySkills.svg";
+import SkillsList from "../../components/SkillsList/SkillsList";
+
 const SkillsPage = () => {
   return (
-    <>
-      <div></div>
-    </>
+    <div className={css.skillsContainer}>
+      <div className={css.titleContainer}>
+        <MySkills className={css.title} />
+      </div>
+      <SkillsList />
+    </div>
   );
 };
 export default SkillsPage;
