a93fea86679e8872d6d5c232284e32577fc1f922
OsMax <www.osmax@gmail.com>
2024-09-10 16:26:35 +0300

    footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,23 @@
+import { NavLink } from "react-router-dom";
 import css from "./Footer.module.css";
+import { ReactComponent as Logo } from "../../assets/svgImg/logo.svg";
 
 const Footer = () => {
-  return <div className={css.footerContainer}>Footer</div>;
+  return (
+    <>
+      <div className={css.footerContainer}>
+        <NavLink className={css.logo} to="/">
+          <Logo width="20" height="20" />
+          MaksOs
+        </NavLink>
+        <a
+          href="mailto:example@example.com"
+          style={{ textDecoration: "none", color: "#fff" }}
+        >
+          www.osmax@gmail.com
+        </a>
+      </div>
+    </>
+  );
 };
 export default Footer;
