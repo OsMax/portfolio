e771adefb12c4085c4d91756763f7f43a0d53070
OsMax <www.osmax@gmail.com>
2024-09-10 14:49:54 +0300

    some edit for project page

--- src/pages/MyProjectPage/MyProjectPage.jsx
+++ src/pages/MyProjectPage/MyProjectPage.jsx
@@ -2,7 +2,13 @@ import ProjectList from "../../components/ProjectList/ProjectList";
 
 const MyProjectPage = () => {
   return (
-    <div>
+    <div style={{ padding: 10 }}>
+      <h2 style={{ color: "#fff", margin: 0, paddingLeft: 10 }}>
+        <span style={{ color: "#C778DD" }}>/</span>progects
+      </h2>
+      <p style={{ margin: 0, color: "#888", fontSize: 12 }}>
+        List of my projects
+      </p>
       <ProjectList />
     </div>
   );
