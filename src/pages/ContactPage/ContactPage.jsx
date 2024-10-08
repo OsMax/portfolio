cc7b8521c1884218a91c88fbf6a9109bc1ee585e
OsMax <www.osmax@gmail.com>
2024-10-08 15:30:09 +0300

    Update ContactPage.jsx

--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -1,4 +1,10 @@
 const ContactPage = () => {
-  return <h1>contacts</h1>;
+  return (
+    <div style={{ padding: 10 }}>
+      <h2 style={{ color: "#fff", margin: 0, paddingLeft: 10 }}>
+        <span style={{ color: "#C778DD" }}>/</span>contacts
+      </h2>
+    </div>
+  );
 };
 export default ContactPage;
