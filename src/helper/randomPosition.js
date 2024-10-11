23885bc061333985d256e5665dace5f5f3f16603
OsMax <www.osmax@gmail.com>
2024-10-11 15:35:10 +0300

    Update randomPosition.js

--- src/helper/randomPosition.js
+++ src/helper/randomPosition.js
@@ -2,11 +2,10 @@ export const randomPosition = (containerWidth, containerHeight, totalItems) => {
   const radius = containerWidth / 3;
   const centerX = containerWidth / 2.5;
   const centerY = containerHeight - 130;
-  const segmentAngle = Math.PI / totalItems; // Угол одного сегмента
+  const segmentAngle = Math.PI / totalItems;
   const positions = [];
 
   for (let i = 0; i < totalItems; i++) {
-    // Генерация случайного угла внутри сегмента
     const angle = segmentAngle * i + Math.random() * segmentAngle;
     const x = centerX + radius * Math.cos(angle);
     const y = centerY + radius * Math.sin(angle);
