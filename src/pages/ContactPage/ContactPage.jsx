f0fcfee1d90b19a95bf72a077829b2a128786c09
OsMax <www.osmax@gmail.com>
2024-10-16 16:16:37 +0300

    Update ContactPage.jsx

--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -1,9 +1,9 @@
-import { ReactComponent as Facebook } from "../../assets/svgImg/burger.svg";
-import { ReactComponent as Linkedin } from "../../assets/svgImg/menu_close.svg";
-import { ReactComponent as Telegram } from "../../assets/svgImg/logo.svg";
-import { ReactComponent as Viber } from "../../assets/svgImg/logo.svg";
-import { ReactComponent as Whatsup } from "../../assets/svgImg/logo.svg";
-import { ReactComponent as Email } from "../../assets/svgImg/logo.svg";
+import { ReactComponent as Facebook } from "../../assets/сontacts/facebook.svg";
+import { ReactComponent as Linkedin } from "../../assets/сontacts/linkedin.svg";
+import { ReactComponent as Telegram } from "../../assets/сontacts/telegram.svg";
+import { ReactComponent as Viber } from "../../assets/сontacts/viber.svg";
+import { ReactComponent as Whatsup } from "../../assets/сontacts/whatsapp.svg";
+import { ReactComponent as Email } from "../../assets/сontacts/email.svg";
 
 const ContactPage = () => {
   return (
@@ -11,7 +11,26 @@ const ContactPage = () => {
       <h2 style={{ color: "#fff", margin: 0, paddingLeft: 10 }}>
         <span style={{ color: "#C778DD" }}>/</span>contacts
       </h2>
-      <li></li>
+      <ul style={{ listStyle: "none" }}>
+        <li>
+          <Facebook width={50} height={50} />
+        </li>
+        <li>
+          <Linkedin width={50} height={50} />
+        </li>
+        <li>
+          <Telegram width={50} height={50} />
+        </li>
+        <li>
+          <Viber width={50} height={50} />
+        </li>
+        <li>
+          <Whatsup width={50} height={50} />
+        </li>
+        <li>
+          <Email width={50} height={50} />
+        </li>
+      </ul>
     </div>
   );
 };
