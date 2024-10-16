7819a211af183661d26c1e358fcfeec74332ee4d
OsMax <www.osmax@gmail.com>
2024-10-16 15:46:59 +0300

    svg connection

--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -1,9 +1,17 @@
+import { ReactComponent as Facebook } from "../../assets/svgImg/burger.svg";
+import { ReactComponent as Linkedin } from "../../assets/svgImg/menu_close.svg";
+import { ReactComponent as Telegram } from "../../assets/svgImg/logo.svg";
+import { ReactComponent as Viber } from "../../assets/svgImg/logo.svg";
+import { ReactComponent as Whatsup } from "../../assets/svgImg/logo.svg";
+import { ReactComponent as Email } from "../../assets/svgImg/logo.svg";
+
 const ContactPage = () => {
   return (
     <div style={{ padding: 10 }}>
       <h2 style={{ color: "#fff", margin: 0, paddingLeft: 10 }}>
         <span style={{ color: "#C778DD" }}>/</span>contacts
       </h2>
+      <li></li>
     </div>
   );
 };
