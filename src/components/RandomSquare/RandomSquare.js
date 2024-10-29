86319614f488f530346105d7dc1314f034992df5
OsMax <www.osmax@gmail.com>
2024-10-29 13:42:14 +0200

    some fix in randomSquare

--- src/components/RandomSquare/RandomSquare.js
+++ src/components/RandomSquare/RandomSquare.js
@@ -1,6 +1,6 @@
 const RandomSquare = () => {
   //   const width = window.innerWidth;
-  const width = window.innerWidth;
+  // const width = window.innerWidth;
   const height = window.innerHeight;
 
   const totalItems = height / 300;
@@ -10,12 +10,12 @@ const RandomSquare = () => {
     const size = Math.floor(Math.random() * (height / 20)) + 50;
     const x = Math.floor(Math.random() * size - size / 2 + 25);
     const y = Math.floor(Math.random() * (height - 100) + 50);
-    positions.push({ top: y, left: -x, size });
+    positions.push({ top: y, left: -x, width: size, height: size });
 
     const sizeR = Math.floor(Math.random() * (height / 20)) + 50;
     const xR = Math.floor(Math.random() * sizeR - sizeR / 2 + 25);
     const yR = Math.floor(Math.random() * (height - 100) + 50);
-    positions.push({ top: yR, left: -xR + width, size });
+    positions.push({ top: yR, right: -xR, width: sizeR, height: sizeR });
   }
 
   return (
@@ -27,11 +27,12 @@ const RandomSquare = () => {
             style={{
               position: "absolute",
               border: "1px solid #555",
-              width: item.size,
-              height: item.size,
-              top: item.top,
-              left: item.left,
+              // width: item.size,
+              // height: item.size,
+              // top: item.top,
+              // left: item.left,
               zIndex: 1,
+              ...item,
             }}
           ></div>
         );
