af6fd6979c1bd9c5c719d5b554e45e2a13151554
OsMax <www.osmax@gmail.com>
2024-11-05 15:21:18 +0200

    ref for matrix component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,16 +1,22 @@
 import { Suspense } from "react";
+import React, { useRef } from "react";
 import { Outlet } from "react-router-dom";
 import RandomSquare from "../RandomSquare/RandomSquare";
 import css from "./Main.module.css";
+import Matrix from "../Matrix/Matrix";
 
 const Main = () => {
+  const containerRef = useRef(null);
+  // console.log(containerRef);
+
   return (
     <>
       <main className={css.main}>
-        <div className={css.mainContainer}>
+        <div className={css.mainContainer} ref={containerRef}>
           <Suspense>
             <Outlet />
           </Suspense>
+          <Matrix containerRef={containerRef} />
         </div>
         <RandomSquare />
       </main>
