97bc421f5ee06a75872882de19d4243498a1eb31
OsMax <www.osmax@gmail.com>
2024-11-27 11:31:50 +0200

    Update MyProjectPage.jsx

--- src/pages/MyProjectPage/MyProjectPage.jsx
+++ src/pages/MyProjectPage/MyProjectPage.jsx
@@ -10,13 +10,23 @@ const MyProjectPage = () => {
         // paddingBottom: 40,
       }}
     >
-      <h2 style={{ color: "#fff", margin: 0, paddingLeft: 10 }}>
-        <span style={{ color: "#C778DD" }}>/</span>progects
-      </h2>
-      <p style={{ margin: 0, paddingLeft: 10, color: "#888", fontSize: 12 }}>
-        List of my projects
-      </p>
-      <ProjectList />
+      <div>
+        <h2 style={{ color: "#fff", margin: 0, paddingLeft: 10 }}>
+          <span style={{ color: "#C778DD" }}>/</span>progects
+        </h2>
+        <p style={{ margin: 0, paddingLeft: 10, color: "#888", fontSize: 12 }}>
+          List of my projects
+        </p>
+        <ProjectList />
+      </div>
+      <div style={{ marginTop: 28 }}>
+        <h2 style={{ color: "#fff", margin: 0, paddingLeft: 10 }}>
+          <span style={{ color: "#C778DD" }}>/</span>components
+        </h2>
+        <p style={{ margin: 0, paddingLeft: 10, color: "#888", fontSize: 12 }}>
+          List of components
+        </p>
+      </div>
     </div>
   );
 };
